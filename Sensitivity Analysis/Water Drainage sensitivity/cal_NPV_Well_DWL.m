bba15e9bcaee4dd7143f83f5ec45e3e7314826a3
SajjadZeinal <112807085+SajjadZeinal@users.noreply.github.com>
2023-03-12 18:32:44 +0100

    Create cal_NPV_Well_DWL.m

---
+++ Sensitivity Analysis/Water Drainage sensitivity/cal_NPV_Well_DWL.m
@@ -0,0 +1,28 @@
+function [NPV,totNPV,QOP,QWp,days,QOP_Water_Drainage,QOPT_Water_Drainage]= cal_NPV_Well_DWL
+%All the prices are calculated based on 2016 prices
+b=2.78;%global inflation rate(percent)
+r0=50;%$/STB
+rsepw=6;%$/STB
+riw=0.5;%$/STB
+rio=1;%$/STB
+rates=dataimport_WELL_DWL;
+days=rates(:,1);
+firstyear=1989;
+totaltime=2016-firstyear;
+tk = totaltime - days./365;
+for ii=2:length(days)
+    delt(ii)=days(ii)-days(ii-1);
+end
+delt=delt';
+QOP=rates(:,3);
+QOP_Water_Drainage=rates(:,4);
+QOPT_Water_Drainage=rates(:,5);
+QWP_Water_Drainage=rates(:,6);
+QWp=rates(:,7);
+NPV=delt.*(QOP*r0-QWp*rsepw-QWP_Water_Drainage*rsepw)./(1+b/100).^tk;
+totNPV=sum(NPV);
+%plot(attempt,totNPV,'--or');
+%xlabel('ITERATION')
+%ylabel('NPV')
+%hold on;
+end
